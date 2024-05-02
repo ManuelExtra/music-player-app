effa82f6e978eb39a1e26b7974f76d8c98f7977d
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-05-02 11:45:05 +0100

    config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,25 @@
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { SplashScreen } from 'expo-router'
+import { useCallback } from 'react'
+import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState'
+
+SplashScreen.preventAutoHideAsync()
 
 const App = () => {
+	useLogTrackPlayerState()
+
+	const handleTrackPlayerLoaded = useCallback(() => {
+		SplashScreen.hideAsync()
+	}, [])
+
+	useSetupTrackPlayer({
+		onLoad: handleTrackPlayerLoaded,
+	})
+
 	return (
 		<SafeAreaProvider>
 			<RootNavigation />
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,5 +1,28 @@
-import { FlatList } from 'react-native'
+import { FlatList, FlatListProps, View } from 'react-native'
+import library from '@/assets/data/library.json'
+import { TrackListItem } from './TrackListItem'
+import { utilStyles } from '@/styles'
+import { Track } from 'react-native-track-player'
 
-export const TracksList = () => {
-	return <FlatList data={[]} renderItem={() => null} />
+export type TrackListProps = Partial<
+	FlatListProps<Track> & {
+		tracks: Track[]
+	}
+>
+
+const ItemDivider = () => {
+	return <View style={{ ...utilStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }}></View>
+}
+
+export const TracksList = ({ tracks, ...flatlistProps }: TrackListProps) => {
+	return (
+		<FlatList
+			data={tracks}
+			contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
+			ListFooterComponent={ItemDivider}
+			ItemSeparatorComponent={ItemDivider}
+			renderItem={({ item: track }: any) => <TrackListItem track={track} />}
+			{...flatlistProps}
+		/>
+	)
 }
